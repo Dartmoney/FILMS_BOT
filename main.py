ec149711fdffdf4c184b795925da4417d8b91a6e
Dartmoney <imakovislam@mail.ru>
2023-01-09 16:25:52 +0300

    начало парсинга бд

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 import random
 import sqlite3
-from seleniumwire import webdriver  # Import from seleniumwire
-from selenium.webdriver.chrome.options import Options
-import time
+from multiprocessing import Process
+
 db = sqlite3.connect("Triangle_Kino.db")
 cur = db.cursor()
+proxy = dict(https='socks5://159.203.13.82:46202')
 
 cur.execute("""CREATE TABLE IF NOT EXISTS Triangle_Kino (
     ID INTEGER PRIMARY KEY,
@@ -25,22 +25,20 @@ user_agent_list = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.119 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.119 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.119 Safari/537.36"
 ]
-
-user_agent = random.choice(user_agent_list)
-headers = {'User-Agent': user_agent}
 
 x = 1
 
-url = "http://hdrezkabbsm2k.net/?filter=last&genre=1"
-while True:
-    try:
-        user_agent = random.choice(user_agent_list)
+
+def req(k, x=0, url="https://rezka.ag/?filter=last&genre=1"):
+    user_agent = random.choice(user_agent_list)
+    while True:
         headers = {'User-Agent': user_agent}
         i = 0
-        time.sleep(0.01)
-        req = []
-        page = BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
+        page = BeautifulSoup(requests.get(url, headers=headers, proxies=proxy).text, "lxml")
         name_list = []
         link1_list = []
         opisanie_list = []
@@ -51,63 +49,57 @@ while True:
             print(url)
             link1_list.append(url)
             name_list.append(name_text)
-    except Exception as e:
-        with open("eror.txt","w") as s:
-            s.write(str(e))
-    for link0 in link1_list:
-        try:
-            time.sleep(0.01)
-            page2 = BeautifulSoup(requests.get(link0, headers=headers).text, "lxml")
+
+        for link0 in link1_list:
+            page2 = BeautifulSoup(requests.get(link0, headers=headers, proxies=proxy).text, "lxml")
             opisanie = page2.find("div", class_="b-post__description_text").text
-                
             print(opisanie)
 
-            options = Options()
-            options.add_argument("--headless")
-            time.sleep(0.01)
-            try:
-                driver = webdriver.Chrome(options=options,executable_path="home/dartmoney/MY_BOT/FILMS_BOT/chromedriver")
-        # Go to the Google home page
-                driver.get(link0)
-        # Access requests via the `requests` attribute
-                for request in driver.requests:
-                    if request.response:
-                        if ".stream.voidboost.cc/" in request.url:
-                            req.append(
-                                request.url
-                            )
-                time.sleep(0.01)
-                driver.quit()
-                opisanie_list.append(opisanie)
-            except Exception as e:
-                print(e)
-                with open("eror.txt","w") as s:
-                    s.write(str(e))
-                time.sleep(0.01)
-                driver.close()
-        except Exception as e:
-            print(e)
-            with open("eror.txt", "w") as s:
-                s.write(str(e))
-    try:
         while i < len(opisanie_list):
+            if not (((i + 1) < len(name_list)) and ((i + 1) < len(link1_list)) and ((i + 1) < len(opisanie_list))):
+                continue
             name1 = name_list[i]
             opisanie1 = opisanie_list[i]
-            link2 = req[i]
+            link2 = link1_list[i]
             cur.execute("""INSERT INTO Triangle_Kino (RESURS, NAME, OPISANIE, LINK_STR) VALUES (?, ?, ?, ?);""",
                         (resursZERO, name1, opisanie1, link2))
             db.commit()
             print("Добавлено " + str(i))
             i = i + 1
         x = x + 1
-        url = f"http://hdrezkabbsm2k.net/page/{x}/?filter=last&genre=1"
-        if x==1008:
+        url = f"https://rezka.ag/page/{x}/?filter=last&genre=1"
+        if x == k:
             break
-    except Exception as e:
-        with open("eror.txt", "w") as s:
-            s.write(str(e))
-            print(e)
-            x = x + 1
-            url = f"http://hdrezkabbsm2k.net/page/{x}/?filter=last&genre=1"
-            if x == 1008:
-                break
+
+
+if __name__ == '__main__':
+    p1 = Process(target=req(100))
+    p1.start()
+    p2 = Process(target=req(200, x=101, url="https://rezka.ag/page/101/?filter=last&genre=1"))
+    p2.start()
+    p3 = Process(target=req(300, x=201, url="https://rezka.ag/page/201/?filter=last&genre=1"))
+    p3.start()
+    p4 = Process(target=req(400, x=301, url="https://rezka.ag/page/301/?filter=last&genre=1"))
+    p4.start()
+    p5 = Process(target=req(500, x=401, url="https://rezka.ag/page/401/?filter=last&genre=1"))
+    p5.start()
+    p6 = Process(target=req(600, x=501, url="https://rezka.ag/page/501/?filter=last&genre=1"))
+    p6.start()
+    p7 = Process(target=req(700, x=601, url="https://rezka.ag/page/601/?filter=last&genre=1"))
+    p7.start()
+    p8 = Process(target=req(800, x=701, url="https://rezka.ag/page/701/?filter=last&genre=1"))
+    p8.start()
+    p9 = Process(target=req(900, x=801, url="https://rezka.ag/page/801/?filter=last&genre=1"))
+    p9.start()
+    p10 = Process(target=req(1009, x=901, url="https://rezka.ag/page/901/?filter=last&genre=1"))
+    p10.start()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
+    p6.join()
+    p7.join()
+    p8.join()
+    p9.join()
+    p10.join()
