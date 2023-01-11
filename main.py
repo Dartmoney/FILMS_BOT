3728c24ad8748c6f9f55642c1ad43fa33b0586ed
Dartmoney <imakovislam@mail.ru>
2023-01-11 14:17:30 +0300

    Парсинг сайта с прокси

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import time
+
 from bs4 import BeautifulSoup
 import requests
 import random
 import sqlite3
 from multiprocessing import Process
-import time
 
 db = sqlite3.connect("Triangle_Kino.db")
 cur = db.cursor()
+
 cur.execute("""CREATE TABLE IF NOT EXISTS Triangle_Kino (
     ID INTEGER PRIMARY KEY,
     RESURS TEXT,
@@ -23,61 +25,102 @@ user_agent_list = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.119 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.119 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.119 Safari/537.36"
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
 ]
 
-Tek_mirror = "http://hdrezka443rtt.net"
+
+def get_free_proxies():
+    url = "https://free-proxy-list.net/"
+    # получаем ответ HTTP и создаем объект soup
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    k = soup.find("table", class_="table table-striped table-bordered")
+    proxies = []
+    p = 0
+    for j in k.find_all("tr")[1:]:
+        row_data = j.find_all("td")
+        row = [row_data[i].text for i in range(2)]
+        proxies.append(":".join(row))
+    return proxies
 
 
-def req(k, x=0, url=f"{Tek_mirror}?filter=last&genre=1"):
+def get_session(proxies):
+    # создать HTTP‑сеанс
+    session = requests.Session()
+    # выбираем один случайный прокси
+    proxy = random.choice(proxies)
+    session.proxies = {"http": proxy, "https": proxy}
+
+    return session
+
+
+proxy = get_free_proxies()
+
+x = 1
+Tek_mirror = "http://hdrezka443rtt.net/"
+
+
+def req(k, x=1, url=f"{Tek_mirror}?filter=last&genre=1"):
     user_agent = random.choice(user_agent_list)
-
     while True:
+        s = get_session(proxy)
         try:
-            headers = {'User-Agent': user_agent}
-            i = 0
-            time.sleep(0.01)
+            print("Страница запроса с IP:", s.get("http://icanhazip.com", timeout=1.5).text.strip())
+            break
+        except Exception as e:
+            continue
+    while True:
+        headers = {'User-Agent': user_agent}
+        i = 0
+        time.sleep(0.01)
+        try:
             page = BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
-            name_list = []
-            link1_list = []
-            opisanie_list = []
         except:
-            continue
-        try:
-            for name in page.find_all("div", class_="b-content__inline_item-link"):
-                name_text = name.text
-                url = name.find_all('a')[0].get("href")
-                print(name_text)
-                print(url)
-                link1_list.append(url)
-                name_list.append(name_text)
-        except:
-            continue
-        for link0 in link1_list:
-            try:
-                time.sleep(0.01)
-                page2 = BeautifulSoup(requests.get(link0, headers=headers).text, "lxml")
-                opisanie = page2.find("div", class_="b-post__description_text").text
-                print(opisanie)
-            except:
-                continue
-        while i < len(opisanie_list):
-            try:
-                if not (((i + 1) < len(name_list)) and ((i + 1) < len(link1_list)) and ((i + 1) < len(opisanie_list))):
+            while True:
+                s = get_session(proxy)
+                try:
+                    print("Страница запроса с IP:", s.get("http://icanhazip.com", timeout=1.5).text.strip())
+                    break
+                except Exception as e:
                     continue
-                name1 = name_list[i]
-                opisanie1 = opisanie_list[i]
-                link2 = link1_list[i]
-                cur.execute("""INSERT INTO Triangle_Kino (RESURS, NAME, OPISANIE, LINK_STR) VALUES (?, ?, ?, ?);""",
-                            (resursZERO, name1, opisanie1, link2))
-                db.commit()
-                print("Добавлено " + str(i))
-                i = i + 1
+        name_list = []
+        link1_list = []
+        opisanie_list = []
+        print(page)
+        print(page.find_all("div", class_="b-content__inline_item-link"))
+        for name in page.find_all("div", class_="b-content__inline_item-link"):
+            name_text = name.text
+            url = name.find_all('a')[0].get("href")
+            print(name_text)
+            print(url)
+            link1_list.append(url)
+            name_list.append(name_text)
+
+        for link0 in link1_list:
+            time.sleep(0.01)
+            try:
+                page2 = BeautifulSoup(requests.get(link0, headers=headers).text, "lxml")
             except:
+                while True:
+                    s = get_session(proxy)
+                    try:
+                        print("Страница запроса с IP:", s.get("http://icanhazip.com", timeout=1.5).text.strip())
+                        break
+                    except Exception as e:
+                        continue
+            opisanie = page2.find("div", class_="b-post__description_text").text
+            print(opisanie)
+
+        while i < len(opisanie_list):
+            if not (((i + 1) < len(name_list)) and ((i + 1) < len(link1_list)) and ((i + 1) < len(opisanie_list))):
                 continue
+            name1 = name_list[i]
+            opisanie1 = opisanie_list[i]
+            link2 = link1_list[i]
+            cur.execute("""INSERT INTO Triangle_Kino (RESURS, NAME, OPISANIE, LINK_STR) VALUES (?, ?, ?, ?);""",
+                        (resursZERO, name1, opisanie1, link2))
+            db.commit()
+            print("Добавлено " + str(i))
+            i = i + 1
         x = x + 1
         url = f"{Tek_mirror}/page/{x}/?filter=last&genre=1"
         if x == k:
