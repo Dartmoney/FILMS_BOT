6bfb31f06b77a1958e2afa05aec5b7d3149a40b4
Dartmoney <imakovislam@mail.ru>
2023-01-12 18:36:10 +0300

    Парсинг сайта с прокси

--- FILMS_BOT.py
+++ FILMS_BOT.py
@@ -15,12 +15,25 @@ dp = Dispatcher(bot)
 opisanie = False
 
 
-@dp.message_handler(commands=['поиск по названию'])
-async def send_welcome(message: types.Message):
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.reply("Мир вам")
+
+
+@dp.message_handler(commands=['поиск_по_названию_или_по_году'])
+async def search_name(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("напиши название")
+    opisanie = False
+    await message.reply("напиши название или год")
+
+
+@dp.message_handler(commands=['поиск_по_описанию'])
+async def search_opisanie(message: types.Message):
+    opisanie = True
+    await message.reply("напиши описание")
+
 
 if not opisanie:
     @dp.message_handler()
@@ -28,24 +41,28 @@ if not opisanie:
         p = message.text
         con = sqlite3.connect("Triangle_Kino.db")
         cur = con.cursor()
-        cur.execute("""
-                            SELECT FROM films 
-                            WHERE genre = (SELECT id FROM genres WHERE title ='комедия' )
+        result = cur.execute(f"""
+                            SELECT * FROM Triangle_Kino 
+                            WHERE NAME LIKE "%{p}%"
                             """)
+        for i in result:
+            await message.reply(i)
         con.commit()
         con.close()
-# driver = webdriver.Chrome(options=options,executable_path="home/dartmoney/MY_BOT/FILMS_BOT/chromedriver")
-#         # Go to the Google home page
-#                 driver.get(link0)
-#         # Access requests via the `requests` attribute
-#                 for request in driver.requests:
-#                     if request.response:
-#                         if ".stream.voidboost.cc/" in request.url:
-#                             req.append(
-#                                 request.url
-#                             )
-#                 time.sleep(0.01)
-#                 driver.quit()
-#                 opisanie_list.append(opisanie)
+else:
+    @dp.message_handler()
+    async def message_from(message: types.Message):
+        p = message.text
+        con = sqlite3.connect("Triangle_Kino.db")
+        cur = con.cursor()
+        result = cur.execute(f"""
+                                    SELECT * FROM Triangle_Kino 
+                                    WHERE OPISANIE LIKE "%{p}%"
+                                    """)
+        for i in result:
+            print(i)
+        con.commit()
+        con.close()
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
