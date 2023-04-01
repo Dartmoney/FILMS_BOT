4a2741fd289a8879be1a7a270156549f9b72a6cb
Dartmoney <imakovislam@mail.ru>
2023-04-02 00:25:40 +0300

    Добавление комментариев в парсер и телеграм бот

--- FILMS_BOT.py
+++ FILMS_BOT.py
@@ -13,53 +13,30 @@ from selenium.webdriver.chrome.options import Options
 import urllib.request
 from aiogram.bot.api import TelegramAPIServer
 
-Path("/home/dartmoney/MY_BOT/FILMS_BOT/tmpfs").mkdir(parents=True, exist_ok=True)
+# подключаемся к локальному АПИ телеграм
 local_server = TelegramAPIServer.from_base('http://localhost:22')
+# передаем значение токена
 API_TOKEN = API
+# хранение всего в Оперативке (после перезагрузки все состояния для бота стираются)
 storage = MemoryStorage()
-# Configure logging
+# Создаем логгинг
 logging.basicConfig(level=logging.INFO)
-# Initialize bot and dispatcher
+# Инициализируем бота и диспетчер
 bot = Bot(token=API_TOKEN, server=local_server)
 dp = Dispatcher(bot, storage=storage)
 opisanie = False
+# текущее зеркало hdrezka
 tek_miror = "http://hdrezkadkrrq2.net"
 
 
-def get_free_proxies():
-    url = "https://free-proxy-list.net/"
-    # получаем ответ HTTP и создаем объект soup
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
-    k = soup.find("table", class_="table table-striped table-bordered")
-    proxies = []
-    p = 0
-    for j in k.find_all("tr")[1:]:
-        row_data = j.find_all("td")
-        row = [row_data[i].text for i in range(2)]
-        proxies.append(":".join(row))
-    return proxies
-
-
-# sdssdds
-def get_session(proxies):
-    # создать HTTP‑сеанс
-    session = requests.Session()
-    # выбираем один случайный прокси
-    proxy = random.choice(proxies)
-    session.proxies = {"http": proxy, "https": proxy}
-
-    return proxy, session
-
-
-proxy = get_free_proxies()
-
-
+# команда старт
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup()
     await message.reply("Мир вам")
 
 
+# команда для поиска по имени
 @dp.message_handler(commands=["search_name"])
 async def chto(message: types.Message):
     global opisanie
@@ -67,6 +44,7 @@ async def chto(message: types.Message):
     await message.reply("По названию")
 
 
+# команда для поиска по описанию
 @dp.message_handler(commands=["search_opisanie"])
 async def kto(message: types.Message):
     global opisanie
@@ -74,52 +52,67 @@ async def kto(message: types.Message):
     await message.reply("По описанию")
 
 
+# команда для поиска рандомного фильма
 @dp.message_handler(commands=["random"])
 async def random(message: types.Message):
     global res
+    # список для результата
     res = []
+    # Подключение к бд
     con = sqlite3.connect("Triangle_Kino.db")
     cur = con.cursor()
+    # запрос в бд
     result = cur.execute(f"""
                                                 SELECT * FROM Triangle_Kino 
                                                 ORDER BY RANDOM()
                                                 LIMIT 1
                                                 """)
+    # цикл для редактирования и занесения данных в список
     for i in result:
         print(i)
+        # добавление в список ответа и изменение ссылки с поправкой на текущее зеркало
         res.append([i[0], i[2], i[3], (tek_miror + "/films/" + i[4].split("/films/")[1])])
+    # создание инлайн клавиатуры
     kbM = types.InlineKeyboardMarkup(row_width=1)
+    # добавлени кнопки для скачивания фильма
     downloads = types.InlineKeyboardButton(text="скачать", callback_data="downloads")
+    # добавление кнопки в клавиатуру
     kbM.add(downloads)
     con.commit()
     con.close()
+    # отправка сообщения пользователю
     await message.reply(res[0][1] + " " + res[0][2] + " " + res[0][3], reply_markup=kbM)
-    print("ti kto")
+
+    # обработчик нажатия на кнопку скачивания
     @dp.callback_query_handler(text="downloads")
     async def dowmloads(query: types.CallbackQuery):
         global res
         global kbM
         options = Options()
         options.add_argument("--headless")
-        # Go to the Google home page
+        # указание местоположения хромдрайвера
         driver = webdriver.Chrome(options=options, executable_path="home/dartmoney/MY_BOT/FILMS_BOT/chromedriver")
-        print(res)
+        # запрос
         driver.get(res[0][3])
-        print("vse ese est")
         oshibka = True
         try:
-            # Access requests via the `requests` attribute
+            # Просматриваем все запросы которые делает сайт и находим среди них прямую ссылку на фильм
             for request in driver.requests:
                 if request.response:
                     if ".stream.voidboost.cc/" in request.url:
+                        # скачивание фильма
                         urllib.request.urlretrieve((((request.url).split("mp4"))[0] + "mp4"),
                                                    "/tmpfs/" + str(query.message.chat.id) + ".mp4")
+                        # делаем паузу что бы не схватить бан
                         time.sleep(60)
                         break
         except:
+            #  в случае ошибки даем ответ пользователь об ошибке
             oshibka = False
             await query.message.reply("ошибка")
+        # закрываем драйвер
         driver.quit()
+        # в случае если ошибки нет отправляем фильм пользователю
         if oshibka:
             video = open("/tmpfs/" + str(query.message.chat.id) + ".mp4", 'rb')
             await bot.send_video(chat_id=query.message.chat.id, video=video)
@@ -127,25 +120,35 @@ async def random(message: types.Message):
             Path.unlink(Path("/tmpfs/" + str(query.message.chat.id) + ".mp4"))
             time.sleep(60)
 
-
+# условие припоискуе по описанию
 if opisanie:
+    # спсиок для получения ответов с бд
     res = []
     k = 0
+    #  Инлайн клавиатура
     kbM = types.InlineKeyboardMarkup(row_width=5)
+    # кнопка которая отправляет пользователя к первому фильму
     first = types.InlineKeyboardButton(text='<<', callback_data="perv")
+    #  кнопка отправляет пользователя к предыдущему фильму
     pred = types.InlineKeyboardButton(text="<", callback_data="pred")
+    # кнопка для скачивания фильма
     download = types.InlineKeyboardButton(text="скачать", callback_data="download")
+    # кнопка для перехода к следующему фильму
     sled = types.InlineKeyboardButton(text=">", callback_data="sled")
+    # кнопка для перехода к последнему фильму
     posl = types.InlineKeyboardButton(text=">>", callback_data="posl")
+    # добавление всех кнопок в клавиатуру
     kbM.add(first, pred, download, sled, posl)
 
 
+    # обработчик при получении сообщения
     @dp.message_handler()
     async def message_from(message: types.Message):
         global res
         global k
         global kbM
         global tek_miror
+        # текст сообщения
         p = message.text
         con = sqlite3.connect("Triangle_Kino.db")
         cur = con.cursor()
@@ -160,6 +163,7 @@ if opisanie:
         con.close()
 
 
+    # обработчик при нажатии кнопки следующего фильма
     @dp.callback_query_handler(text="sled")
     async def sled(query: types.CallbackQuery):
         global res
@@ -170,6 +174,7 @@ if opisanie:
             await query.message.edit_text(res[k][1] + " " + res[k][2] + " " + res[k][3], reply_markup=kbM)
 
 
+    # кнопка для предыдущего фильма
     @dp.callback_query_handler(text="pred")
     async def pred(query: types.CallbackQuery):
         global res
@@ -180,6 +185,7 @@ if opisanie:
             await query.message.edit_text(res[k][1] + " " + res[k][2] + " " + res[k][3], reply_markup=kbM)
 
 
+    # кнопка для первого фильма
     @dp.callback_query_handler(text="perv")
     async def perv(query: types.CallbackQuery):
         global res
@@ -189,6 +195,7 @@ if opisanie:
         await query.message.edit_text(res[k][1] + " " + res[k][2] + " " + res[k][3], reply_markup=kbM)
 
 
+    # кнопка для последнего фильма
     @dp.callback_query_handler(text="posl")
     async def posl(query: types.CallbackQuery):
         global res
@@ -198,6 +205,7 @@ if opisanie:
         await query.message.edit_text(res[k][1] + " " + res[k][2] + " " + res[k][3], reply_markup=kbM)
 
 
+    # кнопка для скачивания
     @dp.callback_query_handler(text="download")
     async def dowmload(query: types.CallbackQuery):
         global res
@@ -228,7 +236,7 @@ if opisanie:
             video.close()
             Path.unlink(Path("/tmpfs/" + str(query.message.chat.id) + ".mp4"))
             time.sleep(60)
-
+# то же самое только для поиска фильма по названию
 else:
     res = []
     k = 0
--- parser.py
+++ parser.py
@@ -1,14 +1,14 @@
 import time
-
 from bs4 import BeautifulSoup
 import requests
 import random
 import sqlite3
 from multiprocessing import Process
 
+# добавляем базу данных в программу
 db = sqlite3.connect("Triangle_Kino.db")
 cur = db.cursor()
-
+# пишем запрос для добавления таблицы в бд если ее там нет
 cur.execute("""CREATE TABLE IF NOT EXISTS Triangle_Kino (
     ID INTEGER PRIMARY KEY,
     RESURS TEXT,
@@ -19,7 +19,7 @@ cur.execute("""CREATE TABLE IF NOT EXISTS Triangle_Kino (
 db.commit()
 
 resursZERO = "HDREZKA"
-
+# тут я просто скопировал с сайта юзер агентов но потом мне стало лень копировать и я написал скрапер
 user_agent_list = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36",
@@ -102,23 +102,28 @@ user_agent_list = [
     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
 ]
-
+# Запрос на сайт с юзер агентами
 zapolnenie = BeautifulSoup(requests.get("https://useragents.ru/stable.html").content, "html.parser")
+# отбираем определенных юзер агентов на сайте в данном случае с оперы
 opera = zapolnenie.find("div", id="wb_Text9")
 k = 0
+# цикл для добавления всех юзер агентов в список выше
 for i in opera:
     k += 1
     if k == 2:
         user_agent_list += str(i)[62:].split("<br/>")
 
 
+# функция для получения бесплатных прокси
 def get_free_proxies():
     url = "https://free-proxy-list.net/"
     # получаем ответ HTTP и создаем объект soup
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    # получаем те самые прокси
     k = soup.find("table", class_="table table-striped table-bordered")
     proxies = []
     p = 0
+    # цикл для добавления проксей в список
     for j in k.find_all("tr")[1:]:
         row_data = j.find_all("td")
         row = [row_data[i].text for i in range(2)]
@@ -126,7 +131,7 @@ def get_free_proxies():
     return proxies
 
 
-# sdssdds
+# функция для создания сессии в прокси
 def get_session(proxies):
     # создать HTTP‑сеанс
     session = requests.Session()
@@ -140,10 +145,13 @@ def get_session(proxies):
 proxy = get_free_proxies()
 
 x = 1
+# текущее зеркало hdrezkа
 Tek_mirror = "http://hdrezka77ftyy.net/"
 
 
+# сам парсинг фильмов с сайта и добавления их в бд
 def req(k, x=1, url=f"{Tek_mirror}?filter=last&genre=1"):
+    # цикл для подключения к сессии прокси
     for i in range(300):
         s = get_session(proxy)
         try:
@@ -151,11 +159,14 @@ def req(k, x=1, url=f"{Tek_mirror}?filter=last&genre=1"):
             break
         except Exception as e:
             ...
+    # цикл с парсером
     while True:
+        # выбираем юзер агента для каждой страницы
         user_agent = random.choice(user_agent_list)
         headers = {'User-Agent': user_agent}
 
         time.sleep(random.uniform(0, 1))
+        # попытка получения страницы с фильмами в случае ошибки выбираем новый прокси
         try:
             page = BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
         except:
@@ -172,6 +183,7 @@ def req(k, x=1, url=f"{Tek_mirror}?filter=last&genre=1"):
         print(page)
         if "503 Service Temporarily Unavailable" in page:
             continue
+        # проходимся по странице собирая названия фильмов и ссылки на них
         for name in page.find_all("div", class_="b-content__inline_item-link"):
             name_text = name.text
             url = name.find_all('a')[0].get("href")
@@ -179,9 +191,11 @@ def req(k, x=1, url=f"{Tek_mirror}?filter=last&genre=1"):
             print(url)
             link1_list.append(url)
             name_list.append(name_text)
-        print("ti kto")
+        # цикл для прохождения по ссылкам всех фильмов со страницы и собирания их описания
         for link0 in link1_list:
+            # остановка парсера что бы не получить бан и не пришлось искать новый прокси
             time.sleep(random.randint(0, 2))
+            # попытка запроса на страницу с фильмом
             try:
                 page2 = BeautifulSoup(requests.get(link0, headers=headers).text, "lxml")
             except:
@@ -192,6 +206,7 @@ def req(k, x=1, url=f"{Tek_mirror}?filter=last&genre=1"):
                         break
                     except Exception as e:
                         ...
+            # попытка получения описания
             try:
                 opisanie = page2.find("div", class_="b-post__description_text").text
                 opisanie_list.append(opisanie)
@@ -199,6 +214,7 @@ def req(k, x=1, url=f"{Tek_mirror}?filter=last&genre=1"):
             except:
                 ...
         i = 0
+        # цикл для добавления в бд все спарсенных данных
         while i < len(opisanie_list):
             name1 = name_list[i]
             opisanie1 = opisanie_list[i]
@@ -208,10 +224,14 @@ def req(k, x=1, url=f"{Tek_mirror}?filter=last&genre=1"):
             db.commit()
             print("Добавлено " + str(i))
             i = i + 1
+        # увеличиваем переменную которая хранила текущую страницу на единицу
         x = x + 1
+        # меняем ссылку для парсинга на следуюущую страницу с фильмом
         url = f"{Tek_mirror}/page/{x}/?filter=last&genre=1"
+        # ограничиваем количество спарсенных страниц что бы парсер не завершился ошибкой
         if x == k:
             break
 
 
+# запуск функции с парсером и переданным параметром страниц которые требуется спарсить
 req(1009)
